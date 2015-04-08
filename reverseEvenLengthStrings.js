826b185ed363aea8e9e6b158a56ebcca5bd8b74c
Sheryl Boughton <sherpbo@gmail.com>
2015-04-08 11:55:06 -0700

    fix #24

---
+++ reverseEvenLengthStrings.js
@@ -0,0 +1,22 @@
+//i know this is scrap, but for the sake of pull request practice.
+//discard me.
+
+var reverseEvenLengthStrings = function(someArray){
+
+  var i;
+  for (i = 0; i < someArray.length; i ++){
+  if i.length%2{
+    return
+  }
+  elseif{
+    return i.split();
+  }
+   var i.split = evenString;
+
+    reversedString = evenString.split.reverse();
+
+    joinedString = reversedString.join()
+
+
+}
+reverseEvenLengthStrings(['cat','elum','horse','jquery'])
